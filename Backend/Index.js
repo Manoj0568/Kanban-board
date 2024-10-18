68e845e12cdccdfbdad58931815a248f8c2f21af
Manoj Manoharan <manojmanoharan255@gmail.com>
2024-10-18 17:22:09 +0530

    final changes done

--- Backend/Index.js
+++ Backend/Index.js
@@ -2,12 +2,18 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectToMongoDB from './DB/connectToMongoDb.js'
 import cardRouter from './routers/card.router.js'
+import path from 'path';
+const __dirname = path.resolve();
 const app = express()
 dotenv.config()
 app.use(express.json())
 const PORT = process.env.PORT || 3000
 
 app.use("/api",cardRouter)
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
